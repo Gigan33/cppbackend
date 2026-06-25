43bc2b71e28cd1f2702c0d039e307fac0f26eaf9
Gigan33 <Reutov250A@yandex.ru>
2026-06-25 23:52:25 +0300

    fix: add missing keywords include and update logging_handler to Boost.Log

--- sprint2/problems/server_logging/solution/src/logger.h
+++ sprint2/problems/server_logging/solution/src/logger.h
@@ -1,14 +1,14 @@
 #pragma once
 #include <boost/log/trivial.hpp>
+#include <boost/log/expressions/keyword.hpp> // КРИТИЧЕСКИЙ ИНКЛУД ДЛЯ МАКРОСА
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/manipulators/add_value.hpp>
 #include <boost/json.hpp>
-#include <string_view>
 
 namespace json = boost::json;
 namespace logging = boost::log;
 
-// Регистрируем ключевое слово для передачи JSON-данных в логгер
+// Теперь макрос отработает без ошибок
 BOOST_LOG_ATTRIBUTE_KEYWORD(additional_data, "AdditionalData", json::value)
 
 class Logger {
--- sprint2/problems/server_logging/solution/src/logging_handler.h
+++ sprint2/problems/server_logging/solution/src/logging_handler.h
@@ -1,62 +1,59 @@
 #pragma once
 #include "logger.h"
+#include <boost/log/trivial.hpp>
+#include <boost/log/utility/manipulators/add_value.hpp>
+#include <boost/json.hpp>
 #include <chrono>
-#include <string>
 
 namespace http_handler {
 
-template<typename Handler>
+template <typename Handler>
 class LoggingHandler {
 public:
-    explicit LoggingHandler(Handler& handler) : handler_(handler) {}
+    explicit LoggingHandler(Handler& next_handler)
+        : next_handler_{next_handler} {}
 
-    template<typename Request, typename Send>
+    template <typename Request, typename Send>
     void operator()(Request&& req, Send&& send) {
+        // 1. Логируем получение запроса
+        boost::json::value req_data{
+            {"ip", "127.0.0.1"}, // В учебных целях или вытащить из endpoint если доступно
+            {"URI", std::string(req.target())},
+            {"method", std::string(req.method_string())}
+        };
+        BOOST_LOG_TRIVIAL(info) << boost::log::add_value(additional_data, req_data)
+                                << "request received";
+
         auto start_time = std::chrono::steady_clock::now();
-        
-        std::string ip = "127.0.0.1";
-        
-        // Логируем запрос
-        {
-            auto& logger = Logger::GetInstance();
-            json::object data;
-            data["ip"] = ip;
-            data["URI"] = std::string(req.target());
-            data["method"] = std::string(req.method_string());
-            logger.LogJson("request received", data);
-        }
-        
-        // Вызываем оригинальный обработчик
-        handler_(std::forward<Request>(req), [this, ip, start_time, &send](auto&& response) {
-            // Логируем ответ
-            {
-                auto& logger = Logger::GetInstance();
-                auto end_time = std::chrono::steady_clock::now();
-                auto response_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-                
-                std::string content_type;
-                auto ct = response[http::field::content_type];
-                if (!ct.empty()) {
-                    content_type = std::string(ct);
-                }
-                
-                json::object data;
-                data["ip"] = ip;
-                data["response_time"] = response_time;
-                data["code"] = response.result_int();
-                if (content_type.empty()) {
-                    data["content_type"] = nullptr;
-                } else {
-                    data["content_type"] = content_type;
-                }
-                logger.LogJson("response sent", data);
+
+        // 2. Оборачиваем send, чтобы перехватить ответ и замерить время
+        auto log_send = [this, start_time, send = std::forward<Send>(send)](auto&& response) mutable {
+            auto end_time = std::chrono::steady_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+
+            std::string content_type = "null";
+            auto it = response.find(boost::beast::http::field::content_type);
+            if (it != response.end()) {
+                content_type = std::string(it->value());
             }
+
+            // Логируем отправку ответа
+            boost::json::value resp_data{
+                {"response_time", duration},
+                {"code", response.result_int()},
+                {"content_type", content_type == "null" ? boost::json::value(nullptr) : boost::json::value(content_type)}
+            };
+            BOOST_LOG_TRIVIAL(info) << boost::log::add_value(additional_data, resp_data)
+                                    << "response sent";
+
             send(std::forward<decltype(response)>(response));
-        });
+        };
+
+        next_handler_(std::forward<Request>(req), std::move(log_send));
     }
 
 private:
-    Handler& handler_;
+    Handler& next_handler_;
 };
 
 } // namespace http_handler
