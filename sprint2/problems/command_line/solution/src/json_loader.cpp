67ca84329c0762ca85597b9b85b282aca2aadf77
Gigan33 <Reutov250A@yandex.ru>
2026-07-08 22:18:04 +0300

    Fix namespace

--- sprint2/problems/command_line/solution/src/json_loader.cpp
+++ sprint2/problems/command_line/solution/src/json_loader.cpp
@@ -103,7 +103,6 @@ model::Game LoadGame(const std::filesystem::path& json_path) {
     if (const auto* maps_ptr = obj.if_contains("maps")) {
         if (maps_ptr->is_array()) {
             for (const auto& map_json : maps_ptr->as_array()) {
-                // ИСПРАВЛЕНО: Добавили helper:: перед вызовом функции
                 game.AddMap(helper::ParseMap(map_json, default_speed));
             }
         }
