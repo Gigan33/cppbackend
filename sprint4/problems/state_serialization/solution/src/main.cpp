643d11e1b28cc1d6948b7d1302cc25e826439742
Gigan33 <Reutov250A@yandex.ru>
2026-08-02 16:41:19 +0300

    buil complete

--- sprint4/problems/state_serialization/solution/src/main.cpp
+++ sprint4/problems/state_serialization/solution/src/main.cpp
@@ -50,40 +50,40 @@ struct Args {
 std::optional<Args> ParseCommandLine(int argc, char* argv[]) {
     namespace po = boost::program_options;
 
-    po::options_description desc{"Allowed options"s};
+    po::options_description desc{"Allowed options"};
 
     Args args;
     desc.add_options()
         ("help,h", "produce help message")
-        ("tick-period,t", po::value<uint64_t>()->value_name("milliseconds"s), "set tick period")
-        ("config-file,c", po::value<std::string>(&args.config_file)->value_name("file"s), "set config file path")
-        ("www-root,w", po::value<std::string>(&args.www_root)->value_name("dir"s), "set static files root")
-        ("randomize-spawn-points", po::bool_switch(&args.randomize_spawn_points), "spawn dogs at random positions");
-        ("state-file", po::value<std::string>(&args.state_file)->value_name("file"s), "set state file path")
-        ("save-state-period", po::value<uint64_t>()->value_name("milliseconds"s), "set state save period");
+        ("tick-period,t", po::value<uint64_t>()->value_name("milliseconds"), "set tick period")
+        ("config-file,c", po::value<std::string>(&args.config_file)->value_name("file"), "set config file path")
+        ("www-root,w", po::value<std::string>(&args.www_root)->value_name("dir"), "set static files root")
+        ("randomize-spawn-points", po::bool_switch(&args.randomize_spawn_points), "spawn dogs at random positions")
+        ("state-file", po::value<std::string>(&args.state_file)->value_name("file"), "set state file path")
+        ("save-state-period", po::value<uint64_t>()->value_name("milliseconds"), "set state save period");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
 
-    if (vm.count("help"s)) {
+    if (vm.count("help")) {
         std::cout << desc << std::endl;
         return std::nullopt;
     }
 
-    if (!vm.count("config-file"s)) {
-        throw std::runtime_error("Config file path is not specified"s);
+    if (!vm.count("config-file")) {
+        throw std::runtime_error("Config file path is not specified");
     }
-    if (!vm.count("www-root"s)) {
-        throw std::runtime_error("Static files root is not specified"s);
-    }
-
-    if (vm.count("tick-period"s)) {
-        args.tick_period = vm["tick-period"s].as<uint64_t>();
+    if (!vm.count("www-root")) {
+        throw std::runtime_error("Static files root is not specified");
     }
 
-    if (vm.count("save-state-period"s)) {
-        args.save_state_period = vm["save-state-period"s].as<uint64_t>();
+    if (vm.count("tick-period")) {
+        args.tick_period = vm["tick-period"].as<uint64_t>();
+    }
+
+    if (vm.count("save-state-period")) {
+        args.save_state_period = vm["save-state-period"].as<uint64_t>();
     }
 
     return args;
